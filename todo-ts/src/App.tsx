3ab60cfd2eb222f41c239de3b476b1e75320c34f
Amninder <amninderbrar04@gmail.com>
2026-02-03 11:59:28 -0600

    useState

--- todo-ts/src/App.tsx
+++ todo-ts/src/App.tsx
@@ -1,12 +1,13 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const name = "PM"
-  return <h1> hello {name}</h1>
-  
+  const [count, setCount] = useState(0);
 
-  
+  return <>
+    <button onClick={() => setCount(count + 1)}> {count} </button>
+  </>
 
   
 }
